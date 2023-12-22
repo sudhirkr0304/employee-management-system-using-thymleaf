87c628cbccc940eb272aa2a0216adcb7d0883358
sudhir kumar <37249975+sudhirkr0304@users.noreply.github.com>
2023-12-22 15:43:18 +0530

    Initial commit

---
+++ src/test/java/com/sudhir/employeemanagementsystemusingthymleaf/EmployeeManagementSystemUsingThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhir.employeemanagementsystemusingthymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemUsingThymleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
